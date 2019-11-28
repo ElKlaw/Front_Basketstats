486f83419c0f50637b5dc83252c81f42d6b3ecd4
ElKlaw <pierremichel804@gmail.com>
2019-11-28 17:03:47 +0100

    Mise en forme de la page d'affichage des équipes et joueurs du club

--- src/app/joueur/vue-joueur-of-club/vue-joueur-of-club.component.ts
+++ src/app/joueur/vue-joueur-of-club/vue-joueur-of-club.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, ViewChild, Input, AfterViewInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ViewChild, Input, AfterViewInit } from '@angular/core';
 import {JoueurService} from '../../shared/joueur.service';
 import {MatPaginator, MatTableDataSource, MatSort, MatPaginatorIntl} from '@angular/material';
 import { Joueur } from 'src/app/shared/joueur';
 import { FrenchMatPaginatorIntl } from 'src/app/component/language/frenchmatpaginatorintl';
 import { Club } from 'src/app/shared/club';
 import { FormControl } from '@angular/forms';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-vue-joueur-of-club',
@@ -13,59 +13,60 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./vue-joueur-of-club.component.css'],
   providers: [
       {provide: MatPaginatorIntl, useClass: FrenchMatPaginatorIntl}
-  ]
+  ],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class VueJoueurOfClubComponent implements OnInit, AfterViewInit {
-    displayedColumns: string[] = ['nom', 'prenom', 'sexe', 'dateNaissance', 'details'];
+export class VueJoueurOfClubComponent implements AfterViewInit {
+    displayedColumns : string[] = ['nom', 'prenom', 'sexe', 'dateNaissance','detail'];
+    dataTable: MatTableDataSource<Joueur>;
+    expandedElement: Joueur | null;
 
-    dataTable = new MatTableDataSource<Joueur>();
-
+    @Input() club: Club;
     @ViewChild(MatSort) sort: MatSort;
     @ViewChild(MatPaginator) paginator: MatPaginator;
-    
-    @Input() club: Club;
-    
+
     nomFilter = new FormControl();
     filteredValues = {
         nom: ''
     };
-    
-    constructor(
-        private route: ActivatedRoute,
-        public joueurService: JoueurService
-    ) { }
 
-    ngOnInit() {
-        // Initialisation des filtres
-        this.initialisationFiltres();
+    constructor(
+        public joueurService: JoueurService
+    ) {
+      this.dataTable = new MatTableDataSource<Joueur>();
     }
-    ngAfterViewInit(): void {
-        this.loadJoueurs();
+
+    ngAfterViewInit() {
+      this.initialisationFiltres();
+      this.loadJoueurs();
     }
 
     loadJoueurs() {
-        this.joueurService.getAllJoueursFromClub(this.club.id).subscribe((data: {}) => {
-            this.dataTable.data = data as Joueur[];
-        });
+      this.joueurService.getAllJoueursFromClub(this.club.id).subscribe((data: {}) => {
+        this.dataTable.data = data as Joueur[];
         this.dataTable.paginator = this.paginator;
         this.dataTable.sort = this.sort;
         this.dataTable.filterPredicate = (data: Joueur, filtersJson: string) => {
             const searchString = JSON.parse(filtersJson);
-            return data.nom.toString().toLowerCase().includes(searchString.nom.toLowerCase()) || 
+            return data.nom.toString().toLowerCase().includes(searchString.nom.toLowerCase()) ||
                 data.prenom.toString().toLowerCase().includes(searchString.nom.toLowerCase()) ;
         };
+      });
     }
-    
+
     initialisationFiltres() {
         this.nomFilter.valueChanges.subscribe((FilterValue) => {
             this.filtre(FilterValue, 'nom');
         });
     }
-    
+
     filtre(filterValue: string, nomColumn: string) {
-        if (filterValue === 'Tous') {
-            filterValue = '';
-        }
         this.filteredValues[nomColumn] = filterValue;
         this.dataTable.filter = JSON.stringify(this.filteredValues);
         if (this.dataTable.paginator) {
--- src/app/shared/joueur.ts
+++ src/app/shared/joueur.ts
@@ -1,7 +1,10 @@
+import { Equipe } from './equipe';
+
 export class Joueur {
     id: number;
     nom: string;
     prenom: string;
     sexe: string;
     dateNaissance: string;
+    equipes: Equipe[];
 }
