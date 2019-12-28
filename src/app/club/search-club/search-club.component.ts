c7fd2ba270adf2271929a940811ee8adc71eabd9
michel-58040 <pierremichel804@gmail.com>
2019-12-28 22:39:52 +0100

    Search club - Ajout aucun résultat + fix gestion page

--- src/app/club/search-club/search-club.component.ts
+++ src/app/club/search-club/search-club.component.ts
@@ -14,40 +14,43 @@ import { FrenchMatPaginatorIntl } from 'src/app/component/language/frenchmatpagi
   ]
 })
 export class SearchClubComponent implements OnInit {
+  clubs: Club[]= [];
+
   length = 100;
   pageSize = 12;
   pageSizeOptions: number[] = [9, 12, 24];
   @ViewChild('paginatorTop', {static: false}) paginatorTop;
   @ViewChild('paginatorBottom', {static: false}) paginatorBottom;
-  @ViewChild('search-input', {static: false}) searchClub;
+  @ViewChild('searchInput', {static: false}) searchClub;
 
-  clubs: Club[]= [];
   constructor(
       public clubService: ClubService
   ) { }
 
   ngOnInit() {
-      this.loadClubs('', 0, this.pageSize);
+    this.loadClubs('', 0, this.pageSize);
   }
 
   searchClubs(nom) {
-      this.loadClubs(nom, 0, this.pageSize)
+    this.loadClubs(nom, 0, this.pageSize)
   }
 
   loadClubs(nom, page, size) {
-      return this.clubService.searchClub(nom, page, size).subscribe((data: Page) => {
-          this.clubs = data.content;
-          this.length= data.totalElements;
-      });
+    return this.clubService.searchClub(nom, page, size).subscribe((data: Page) => {
+      this.clubs = data.content;
+      this.length= data.totalElements;
+    });
   }
 
-  onChangePageBottom(event){
-      this.paginatorTop.pageIndex = event.pageIndex;
-      this.loadClubs(this.searchClub.value, event.pageIndex, event.pageSize);
-  }
+  onChangePage(event){
+    this.paginatorTop.pageIndex = event.pageIndex;
+    this.paginatorTop.pageSize = event.pageSize;
 
-  onChangePageTop(event){
-      this.paginatorBottom.pageIndex = event.pageIndex;
-      this.loadClubs(this.searchClub.value, event.pageIndex, event.pageSize);
+    this.paginatorBottom.pageIndex = event.pageIndex;
+    this.paginatorBottom.pageSize = event.pageSize;
+
+    this.pageSize = event.pageSize;
+
+    this.loadClubs(this.searchClub.nativeElement.value, event.pageIndex, event.pageSize);
   }
 }
